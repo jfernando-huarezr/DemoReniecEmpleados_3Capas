d5b31017bf7dbafe8d0c6858cdff8b2bc5b54af3
Jose Fernando Huarez Reyes <fernando.huarezr@live.com>
2024-05-19 18:53:51 -0500

    entidad Empleado

--- ReniecEmpleados_BE/Class1.cs
+++ ReniecEmpleados_BE/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace ReniecEmpleados_BE
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ReniecEmpleados_BE
+{
+    internal class Class1
+    {
     }
 }
---
+++ ReniecEmpleados_BE/EmpleadoBE.cs
@@ -0,0 +1,25 @@
+﻿namespace ReniecEmpleados_BE
+{
+    public class EmpleadoBE
+    {
+        public String Cod_emp { get; set; }
+        public String Nom_emp { get; set; }
+        public String Ape_emp { get; set; }
+        public String Dni_emp { get; set; }
+        public String Id_Ubigeo { get; set; }
+        public String Tel_emp { get; set; }
+        public String Cor_emp { get; set; }
+        public String NivelAc_emp { get; set; }
+        public String Cod_sed { get; set; }
+        public String Est_civ { get; set; }
+        public DateTime Fech_ingreso { get; set; }
+        public String Sex_emp { get; set; }
+        public DateTime Fech_nacimiento { get; set; }
+        public String Id_puesto { get; set; }
+        public String Id_jefe { get; set; }
+        public byte[] Foto_emp { get; set; }
+        public DateTime Fecha_registro { get; set; }
+        public String Usuario_Ult_Modif { get; set; }
+        public Int16 Estado_emp { get; set; }
+    }
+}
