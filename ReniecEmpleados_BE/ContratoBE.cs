9b324d9888b1b26afb2f9356a1a8fa78806eef2d
Jose Fernando Huarez Reyes <fernando.huarezr@live.com>
2024-05-19 18:59:00 -0500

    entidad ContratoBE

---
+++ ReniecEmpleados_BE/ContratoBE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReniecEmpleados_BE
+{
+    public class ContratoBE 
+    {
+        public String Cod_con { get; set; }
+        public String Cod_emp { get; set; }
+        public DateTime Fec_ini { get; set; }
+        public DateTime Fec_fin { get; set; }
+        public String Usuario_registro { get; set; }
+        public DateTime Fecha_Ult_Modif { get; set; }
+        public String Usuario_Ult_Modif { get; set; }
+        public Int16 Estado_con { get; set; }
+    }
+}
